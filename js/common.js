855bcc4ec29210adf53b045d945301a2c8c08b9b
Vitaliy Pustovyy <vetalsd2@gmail.com>
2017-01-27 21:13:20 +0200

    27.01

--- js/common.js
+++ js/common.js
@@ -9,24 +9,21 @@ $(document).ready(function() {
 	$('.popup').magnificPopup({
 		type: 'inline'
 	});
-	/*
-
-
 	//mask
 	jQuery(function($){
 		$('.phone').mask('+38(099) 999-9999');
 	});
-	*/
+	// tooltip
+	$("a.toolt").tooltip();
 
 
-
-	/*
+	
 	//slick carousel
-	$('.photo_carousel').slick({
-		slidesToShow: 5,
+	$('.chainsaw_slider').slick({
+		slidesToShow: 4,
 		slidesToScroll: 1,
-		asNavFor: '.photo_carousel_for',
-		focusOnSelect: true,
+		prevArrow: '.chainsaw_prev',
+		nextArrow: '.chainsaw_next',
 		responsive: [
 			{
 			breakpoint: 992,
@@ -35,7 +32,78 @@ $(document).ready(function() {
 			}
 			}
 		]
-	});*/
+	});
+	$('.brushcutters_slider').slick({
+		slidesToShow: 4,
+		slidesToScroll: 1,
+		prevArrow: '.brushcutters_prev',
+		nextArrow: '.brushcutters_next',
+		responsive: [
+			{
+			breakpoint: 992,
+			settings: {
+				slidesToShow: 3
+			}
+			}
+		]
+	});
+	$('.trimmers_slider').slick({
+		slidesToShow: 4,
+		slidesToScroll: 1,
+		prevArrow: '.trimmers_prev',
+		nextArrow: '.trimmers_next',
+		responsive: [
+			{
+			breakpoint: 992,
+			settings: {
+				slidesToShow: 3
+			}
+			}
+		]
+	});
+	$('.clear_slider').slick({
+		slidesToShow: 4,
+		slidesToScroll: 1,
+		prevArrow: '.clear_prev',
+		nextArrow: '.clear_next',
+		responsive: [
+			{
+			breakpoint: 992,
+			settings: {
+				slidesToShow: 3
+			}
+			}
+		]
+	});
+	$('.concretecutters_slider').slick({
+		slidesToShow: 4,
+		slidesToScroll: 1,
+		prevArrow: '.concretecutters_prev',
+		nextArrow: '.concretecutters_next',
+		responsive: [
+			{
+			breakpoint: 992,
+			settings: {
+				slidesToShow: 3
+			}
+			}
+		]
+	});
+	$('.cloth_slider').slick({
+		slidesToShow: 4,
+		slidesToScroll: 1,
+		prevArrow: '.cloth_prev',
+		nextArrow: '.cloth_next',
+		responsive: [
+			{
+			breakpoint: 992,
+			settings: {
+				slidesToShow: 3
+			}
+			}
+		]
+	});
+	
 	/*
 	// photo magnific gallery
 	$('.photo_popup').magnificPopup({
